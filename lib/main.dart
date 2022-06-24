9186767c08b232939b61688926f9d610b31f02be
ardiandarmansah <ardiandarmansah9@gmail.com>
2022-06-24 15:15:48 +0700

    Tugas Mobile

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:tokokita/helpers/user_info.dart';
+import 'package:tokokita/ui/login_page.dart';
+import 'package:tokokita/ui/produk_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget page = const CircularProgressIndicator();
+
+  @override
+  void initState() {
+    super.initState();
+    isLogin();
+  }
+
+  void isLogin() async {
+    var token = await UserInfo().getToken();
+    if (token != null) {
+      setState(() {
+        page = const ProdukPage();
+      });
+    } else {
+      setState(() {
+        page = const LoginPage();
+      });
+    }
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Toko Kita',
+      debugShowCheckedModeBanner: false,
+      home: page,
+    );
+  }
+}
